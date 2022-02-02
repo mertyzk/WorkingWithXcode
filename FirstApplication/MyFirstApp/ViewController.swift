2051966f775dd6cca93a097586c49544e712b4c3
RinKusChi <meyuzak@gmail.com>
2022-02-02 12:23:21 +0300

    2 February 2022

---
+++ FirstApplication/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Macbook Air on 2.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
