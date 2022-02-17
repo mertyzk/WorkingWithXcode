baeb1afb01b4e9e74e47029841a6d3307e825573
RinKusChi <meyuzak@gmail.com>
2022-02-17 15:23:14 +0300

    17 February 2022

---
+++ DesignWithCode/DesignWithCode/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  DesignWithCode
+//
+//  Created by Macbook Air on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        myLabel.text = "Text Label Code"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height / 2 - 50/2, width: width * 0.8 , height: 50)
+        view.addSubview(myLabel)
+        
+        let myButton = UIButton()
+        myButton.setTitle("CLICK", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.orange, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.6, width: 200, height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(myAction), for: UIControl.Event.touchUpInside)
+        
+    }
+    
+    @objc func myAction(){
+        myLabel.text = "Clicked"
+    }
+
+
+}
+
